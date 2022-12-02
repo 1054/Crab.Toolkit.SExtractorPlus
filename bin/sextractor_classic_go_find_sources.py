251de6266df8459333f50eddf60d457f543184a6
1054 <>
2022-12-02 23:47:41 +0100

    Updates

--- bin/sextractor_classic_go_find_sources.py
+++ bin/sextractor_classic_go_find_sources.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-# 
-
-# binary: sextractor_classic
+"""
+Run SExtractor classic, output to a subdirectory '{input_filename}_run_sextractor_classic_dir'
+"""
 
 import os, sys, re, copy, glob, shutil
 import click
@@ -20,14 +20,18 @@ from astropy.wcs.utils import proj_plane_pixel_area
 @click.argument('image_file')
 @click.option('--detect-thresh', type=float, default=10.0, help='DETECT_THRESH in sigma.')
 @click.option('--analysis-thresh', type=float, default=4.0, help='ANALYSIS_THRESH in sigma.')
-@click.option('--detect-minradius', type=float, default=0.25, help='Set a min radius in arcsec to convert it to DETECT_MINAREA with pi * r^2.')
+@click.option('--detect-minradius', type=float, default=0.2, help='Set a min radius in arcsec to convert it to DETECT_MINAREA with pi * r^2.')
 @click.option('--phot-apertures', type=float, default=1.5, help='PHOT_APERTURES in arcsec')
+@click.option('--make-plot', is_flag=True, default=True, help='make plot')
+@click.option('--overwrite', is_flag=True, default=False, help='overwrite')
 def main(
         image_file,
         detect_thresh,
         analysis_thresh,
         detect_minradius,
         phot_apertures,
+        make_plot,
+        overwrite,
     ):
     
     script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -72,6 +76,12 @@ def main(
             sci_data[mask_zero_weight] = np.nan
             rms_data[mask_zero_weight] = np.nan
     
+    # mask out zero-rms pixels
+    if rms_data is not None:
+        mask_zero_rms = ~np.isfinite(rms_data)
+        if np.count_nonzero(mask_zero_rms) > 0:
+            sci_data[mask_zero_rms] = np.nan
+    
     # get wcs
     wcs = WCS(sci_header, naxis=2)
     pixsc = np.sqrt(proj_plane_pixel_area(wcs))*3600.0 # arcsec
@@ -86,18 +96,24 @@ def main(
     if not os.path.isfile(sci_file):
         hdul = fits.HDUList([fits.PrimaryHDU(header=main_header), fits.ImageHDU(data=sci_data, header=sci_header, name='SCI')])
         hdul.writeto(sci_file, overwrite=True)
+    else:
+        print('Found existing file {!r}. Skipping.'.format(sci_file))
     
     if wht_data is not None:
         wht_file = os.path.join(working_dir, 'wht_data.fits')
         if not os.path.isfile(wht_file):
             hdul = fits.HDUList([fits.PrimaryHDU(header=main_header), fits.ImageHDU(data=wht_data, header=wht_header, name='WHT')])
             hdul.writeto(wht_file, overwrite=True)
+        else:
+            print('Found existing file {!r}. Skipping.'.format(wht_file))
     
     if rms_data is not None:
         rms_file = os.path.join(working_dir, 'rms_data.fits')
         if not os.path.isfile(rms_file):
             hdul = fits.HDUList([fits.PrimaryHDU(header=main_header), fits.ImageHDU(data=rms_data, header=rms_header, name='RMS')])
             hdul.writeto(rms_file, overwrite=True)
+        else:
+            print('Found existing file {!r}. Skipping.'.format(rms_file))
     
     # copy param files
     for default_filename in ['default.conv', 'default.param', 'default.sex']:
@@ -109,7 +125,7 @@ def main(
     # check output file
     out_catalog_file = os.path.join(working_dir, 'SExtractor_OutputCatalog.fits')
     
-    if not os.path.isfile(out_catalog_file):
+    if overwrite or not os.path.isfile(out_catalog_file):
         
         # prepare run_args
         run_args = ['sex', 'sci_data.fits', '-c', 'default.sex']
@@ -196,15 +212,18 @@ def main(
     
     else:
         
-        print('Found catalog: {!r}. Will not re-run SExtractor.'.format(out_catalog_file))
+        print('Found existing file: {!r} and overwrite is False. Will not re-run SExtractor.'.format(out_catalog_file))
         
+    
+    # Read output catalog
+    out_catalog = Table.read(out_catalog_file)
+    
     
     # reformat out_catalog_file
     # output a ds9 region and a catfile
     out_region_file = os.path.join(working_dir, 'SExtractor_OutputCatalog.ds9.reg')
     out_catfile = os.path.join(working_dir, 'SExtractor_OutputCatalog.csv')
-    if not os.path.isfile(out_region_file) or not os.path.isfile(out_catfile):
-        out_catalog = Table.read(out_catalog_file)
+    if overwrite or not os.path.isfile(out_region_file) or not os.path.isfile(out_catfile):
         with open(out_region_file, 'w') as fp:
             fp.write('# DS9 Region file\n')
             fp.write('image\n')
@@ -223,6 +242,66 @@ def main(
                     out_catalog['YWIN_IMAGE'][i]
                 ))
             print('Output to catfile for tweakreg: {!r}.'.format(out_catfile))
+    
+    else:
+        
+        print('Found existing file: {!r} and overwrite is False. Skipping.'.format(out_region_file))
+    
+    
+    # make plot
+    out_figure_file = os.path.join(working_dir, 'SExtractor_OutputCatalog.quicklook.png')
+    if make_plot and overwrite or not os.path.isfile(out_figure_file):
+        import matplotlib as mpl
+        mpl.rcParams['axes.labelsize'] = '9' # https://matplotlib.org/users/customizing.html
+        mpl.rcParams['axes.grid'] = False
+        mpl.rcParams['axes.axisbelow'] = True
+        mpl.rcParams['axes.labelpad'] = 10
+        mpl.rcParams['font.size'] = 10
+        mpl.rcParams['font.family'] = 'Monaco'
+        mpl.rcParams['grid.color'] = 'white'
+        mpl.rcParams['grid.linewidth'] = 0.8
+        mpl.rcParams['grid.alpha'] = 0.8
+        mpl.rcParams['legend.fontsize'] = '10'
+        mpl.rcParams['legend.borderaxespad'] = 0.2 # space between legend border and axis
+        mpl.rcParams['mathtext.fontset'] = 'cm'
+        mpl.rcParams['savefig.dpi'] = 300
+        import matplotlib.pyplot as plt
+        import matplotlib.ticker as ticker
+        import matplotlib.gridspec as gridspec
+        import matplotlib.patheffects as path_effects
+        import matplotlib.cm as mplcm
+        import matplotlib.colors as mplcolors
+        from matplotlib.patches import Ellipse, Circle
+        from mpl_toolkits.axes_grid1 import make_axes_locatable
+        from astropy.visualization import simple_norm, ZScaleInterval
+        image = sci_data
+        ny, nx = sci_data.shape
+        aspect = float(ny)/float(nx)
+        fig = plt.figure(figsize=(10., 10./nx*ny))
+        ax = fig.add_subplot(1,1,1)
+        zscale_interval = ZScaleInterval()
+        min_cut, max_cut = zscale_interval.get_limits(image)
+        norm = simple_norm(image, 'linear', min_cut=min_cut, max_cut=max_cut)
+        ax.imshow(image, origin='lower', interpolation='nearest', 
+                  norm=norm, cmap='gray', aspect=1)
+        
+        # plot regions
+        for i in range(len(out_catalog)):
+            x = out_catalog['XWIN_IMAGE'][i]
+            y = out_catalog['YWIN_IMAGE'][i]
+            rad = out_catalog['KRON_RADIUS'][i]
+            circ = Circle([x, y], rad*2.0, edgecolor='cyan', facecolor='none')
+            ax.add_artist(circ)
+        
+        # draw
+        fig.canvas.draw()
+        
+        # savefig
+        fig.savefig(out_figure_file)
+    
+    elif make_plot:
+        
+        print('Found existing file: {!r} and overwrite is False. Skipping.'.format(out_figure_file))
     
     
     # end of main()
